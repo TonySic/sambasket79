f453612dd0e5402cd07685c9ecf25212ec07b5a1
TonySic <nanotsic@gmail.com>
2023-02-16 12:10:46 +0100

    migrations corrigées

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,7 +18,7 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'nom' => fake()->name(),
+            'nom' => fake()->lastName(),
             'prenom' => fake()->firstName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
